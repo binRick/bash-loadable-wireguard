8dfc39824e8e50adc012f9ff1eb235d632f051c7
root <root@f35.vpnservice.company>
2021-12-04 13:17:10 -0500

    m

--- src/reproc/src/error.posix.c
+++ src/reproc/src/error.posix.c
@@ -1,13 +1,9 @@
 #define _POSIX_C_SOURCE 200809L
-
 #include "error.h"
-
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <reproc/reproc.h>
-
 #include "macro.h"
 
 const int REPROC_EINVAL = -EINVAL;
@@ -18,10 +14,8 @@ const int REPROC_EWOULDBLOCK = -EWOULDBLOCK;
 
 enum { ERROR_STRING_MAX_SIZE = 512 };
 
-const char *error_string(int error)
-{
+const char *error_string(int error) {
   static THREAD_LOCAL char string[ERROR_STRING_MAX_SIZE];
-//  int r = 
   strerror_r(abs(error), string, ARRAY_SIZE(string));
   if (error != 0) {
     return "Failed to retrieve error string";
