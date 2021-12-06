d6bfc2bd0e941a9d038633671b432fb58d9b2b0b
root <root@f35.vpnservice.company>
2021-12-06 17:19:11 -0500

    m

--- src/term_demo.c
+++ src/term_demo.c
@@ -1,25 +1,34 @@
 
 int term_demo(int argc, char **argv){
+  term_reset();
+//  term_clear();
   term_bold();
   term_underline();
   printf("bold and underlined\n");
 
- // term_reset();
- // printf("reset\n");
+  printf("reset\n");
 
+  term_bright();
   term_color("green");
   printf("ok\n");
 
-  term_bold();
-  term_color("red");
-  term_background("red");
-  printf("fail");
+//  term_clear();
   term_reset();
+  term_bold();
+  term_blink();
+  term_underline();
+  term_color("red");
+  term_background("black");
+  printf("fail\n");
+  term_reverse();
+  printf("fail\n");
+  term_reset();
+//  term_clear();
 
 //  term_move_to(50, 10);
 //  printf("hey");
 //  term_move_by(1, 1);
-//  printf("there");
+ // printf("there");
 //  term_move_to(0, 15);
 
 //  int w = 0, h = 0;
